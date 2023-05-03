89aedb41d8577670e1d42a4dff0dd35fb2f6eeab
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-03 00:21:57 -0400

    Add toggle to button to decide if dog is good or bad

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function displayDogs(dog) {
     dogSpan.textContent = dog.name;
     dogBar.appendChild(dogSpan);
 
-    dogSpan.addEventListener("click", () => displayInfo(dog, dogSpan));
+    dogSpan.addEventListener("click", () => displayInfo(dog));
 }
 
 function displayInfo(dog) {
@@ -29,8 +29,31 @@ function displayInfo(dog) {
 
     dogImage.setAttribute("src", dog.image);
     dogName.textContent = dog.name;
-    dog.isGoodDog === true ? dogBehavior.textContent = "Good Dog!" : dogBehavior.textContent = "Bad Dog!";
+    dogGoodOrBad(dog, dogBehavior);
 
     dogInfo.append(dogImage, dogName, dogBehavior);
     dogContainer.appendChild(dogInfo);
+
+    dogBehavior.addEventListener("click", () => toggleBehavior(dog, dogBehavior));
+}
+
+function toggleBehavior(dog, behavior) {
+    dog.isGoodDog = !dog.isGoodDog
+    fetch(`http://localhost:3000/pups/${dog.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            isGoodDog: dog.isGoodDog
+        })
+    })
+        .then(res => res.json())
+        .then(dog => dogGoodOrBad(dog, behavior))
+        .catch(err => console.log(err))
+}
+
+function dogGoodOrBad(dog, behavior) {
+    dog.isGoodDog === true ? behavior.textContent = "Good Dog!" : behavior.textContent = "Bad Dog!";
 }
